0fedcec822fbf55a5d9a8be43105824a82639bc1
Alex Manet <alexander.manap@gmail.com>
2023-02-22 15:08:50 +0100

    Añadido CROSS-ENV

--- database.js
+++ database.js
@@ -1,10 +1,19 @@
 const pool = require('mysql2/promise')
 require('dotenv').config()
 
-module.exports= pool.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-})
-
+if(process.env.NODE_ENV==='develop'){
+    module.exports= pool.createPool({
+        host: process.env.DEVELOPHOST,
+        user: process.env.DEVELOPUSER,
+        port: process.env.DEVELOPPORT,
+        password: process.env.DEVELOPPASSWORD,
+        database: process.env.DEVELOPDATABASE
+    })
+} else {
+    module.exports= pool.createPool({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    })
+}
