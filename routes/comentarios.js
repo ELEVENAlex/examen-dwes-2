20e6b8b7e618990793f0be0fbb91d8c3ac70dc3e
Alex Manet <alexander.manap@gmail.com>
2023-02-22 12:30:15 +0100

    Creada función de ver comentarios

--- routes/comentarios.js
+++ routes/comentarios.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const pool = require('../database')
 
 
+router.get('/comentarios/:id', async (req, res) => {
+    const {id} = req.params
+    const [rows] = await pool.query('SELECT * FROM comentarios WHERE id_imagen = ?', [id])
+    res.render('comentarios', {comentarios: rows})
+})
+
 
 
 
