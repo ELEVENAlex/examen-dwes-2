653bcdc96203deec82ba90f055174adbf135a0dd
Alex Manet <alexander.manap@gmail.com>
2023-02-22 12:47:46 +0100

    Creada función de añadir comentarios

--- routes/comentarios.js
+++ routes/comentarios.js
@@ -7,7 +7,24 @@ const pool = require('../database')
 router.get('/comentarios/:id', async (req, res) => {
     const {id} = req.params
     const [rows] = await pool.query('SELECT * FROM comentarios WHERE id_imagen = ?', [id])
-    res.render('comentarios', {comentarios: rows})
+    res.render('comentarios', {comentarios: rows, id})
+})
+
+router.get('/addcomment/:id', async (req, res) => {
+    const {id} = req.params
+    res.render('addcomment', {id})
+})
+
+router.post('/addcomment/:id', async (req, res) => {
+    const {id} = req.params
+    const {autor, comentario} = req.body
+    const newComentario = {
+        autor,
+        comentario,
+        id_imagen: id
+    }
+    await pool.query('INSERT INTO comentarios SET ?',[newComentario])
+    res.redirect('/fotos/comentarios/' + id)
 })
 
 
